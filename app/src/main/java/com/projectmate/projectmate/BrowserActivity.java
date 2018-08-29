340526b02d0c07d01de52f23f75cf74329eafe7a
Parth Rathore <parth.rathore15@gmail.com>
2018-08-29 22:44:54 +0530

    Added Browser Activity to open codechef

--- app/src/main/java/com/projectmate/projectmate/BrowserActivity.java
+++ app/src/main/java/com/projectmate/projectmate/BrowserActivity.java
@@ -2,12 +2,24 @@ package com.projectmate.projectmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.webkit.WebView;
+
 
 public class BrowserActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
+
+        ActionBar toolbar = getSupportActionBar();
+        toolbar.setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitle("CodeChef Login");
+        toolbar.setElevation(4);
+
+        webView = findViewById(R.id.webview);
     }
 }
