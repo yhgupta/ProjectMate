8692b292b6653b342d612baf702c63cbfba5d7c2
Parth Rathore <parth.rathore15@gmail.com>
2018-09-04 15:13:44 +0530

    Last commit without JSON Parser

--- app/src/main/java/com/projectmate/projectmate/ProfileActivity.java
+++ app/src/main/java/com/projectmate/projectmate/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.projectmate.projectmate;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
+
+        ActionBar toolbar = getSupportActionBar();
+        toolbar.setTitle("Your Profile");
+        toolbar.setDisplayHomeAsUpEnabled(true);
+        }
 }
